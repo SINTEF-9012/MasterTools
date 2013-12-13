b20a9cb0f92c9706c70d8b22db19b4d89b0ce369
Antoine Pultier <antoine.pultier@sintef.no>
2013-12-13 13:20:46 +0100

    DataBase :-)

--- recorder.ts
+++ recorder.ts
@@ -4,12 +4,9 @@
 // Import in typescript and commondjs style
 var ProtoBuf = require("protobufjs");
 var ws = require("ws");
+var sqlite3 = require('sqlite3').verbose();
 
-var protoTransaction : NodeMaster.TransactionBuilder =
-	ProtoBuf.protoFromFile("./definitions/NodeMaster.proto")
-		.build("NodeMaster.Transaction");
-
-var dataBase : {
+var scope : {
 	HelpBeaconStore : {[ID : string] : NodeMaster.HelpBeaconModel };
 	IncidentObjectStore : {[ID : string] : NodeMaster.IncidentObjectModel };
 	MediaStore : {[ID : string] : NodeMaster.MediaModel };
@@ -27,17 +24,41 @@ var dataBase : {
 	ResourceStatusStore: {}
 };
 
-
 // Update each no-null properties
 function updateProperties(input: any, output:any) {
 	for (var key in input) {
-		console.log(key); // TODO debug
-		
-		if (input[key] != null) {
-			output[key] = input[key];
+		// Don't update getter and setters
+		if (!key.match(/^[sg]et/)) {
+			console.log(key); // TODO debug
+			
+			if (input[key] != null) {
+				output[key] = input[key];
+			}
 		}
 	}
 }
+
+function StoreToList(input: {[ID:string] : any}) : any[] {
+	var list = [];
+	for (var key in input) {
+		list.push(input[key]);
+	}
+	return list;
+}
+
+var db = new sqlite3.Database('recorder.db');
+
+db.serialize(function() {
+	db.run('CREATE TABLE IF NOT EXISTS recorder (datetime INTEGER, scope BLOB)');
+	db.run('CREATE UNIQUE INDEX IF NOT EXISTS recorderindex ON recorder(datetime)');
+});
+
+var insertDb = db.prepare('INSERT INTO recorder VALUES (?, ?)'),
+	findDb = db.prepare('SELECT datetime, scope FROM recorder WHERE ABS($time-datetime) = (select MIN(ABS($time - datetime)) from recorder)');
+
+var protoTransaction : NodeMaster.TransactionBuilder =
+	ProtoBuf.protoFromFile("./definitions/NodeMaster.proto")
+		.build("NodeMaster.Transaction");
 
 var wsi = new ws("ws://localhost:8181/MasterTools");
 
@@ -55,86 +76,100 @@ wsi.on('message', function(data : NodeBuffer, flags : any) {
 
 	if (remove) {
 		publish.HelpBeaconList.forEach(function(HelpBeacon) {
-			delete dataBase.HelpBeaconStore[HelpBeacon.ID];
+			delete scope.HelpBeaconStore[HelpBeacon.ID];
 		});
 		publish.IncidentObjectList.forEach(function(IncidentObject) {
-			delete dataBase.IncidentObjectStore[IncidentObject.ID];
+			delete scope.IncidentObjectStore[IncidentObject.ID];
 		});
 		publish.MediaList.forEach(function(Media) {
-			delete dataBase.MediaStore[Media.ID];
+			delete scope.MediaStore[Media.ID];
 		});
 		publish.MessengerList.forEach(function(Messenger) {
-			delete dataBase.MessengerStore[Messenger.ID];
+			delete scope.MessengerStore[Messenger.ID];
 		});
 		publish.PatientList.forEach(function(Patient) {
-			delete dataBase.PatientStore[Patient.ID];
+			delete scope.PatientStore[Patient.ID];
 		});
 		publish.ResourceMobilizationList.forEach(function(ResourceMobilization) {
-			delete dataBase.ResourceMobilizationStore[ResourceMobilization.ID];
+			delete scope.ResourceMobilizationStore[ResourceMobilization.ID];
 		});
 		publish.ResourceStatusList.forEach(function(ResourceStatus) {
-			delete dataBase.ResourceStatusStore[ResourceStatus.ID];
+			delete scope.ResourceStatusStore[ResourceStatus.ID];
 		});
 	}
 
 	if (publish) {
 		publish.HelpBeaconList.forEach(function(HelpBeacon) {
-			var data = dataBase.HelpBeaconStore[HelpBeacon.ID];
+			var data = scope.HelpBeaconStore[HelpBeacon.ID];
 			if (data) {
 				updateProperties(HelpBeacon, data);
 			} else {
-				dataBase.HelpBeaconStore[HelpBeacon.ID] = HelpBeacon;
+				scope.HelpBeaconStore[HelpBeacon.ID] = HelpBeacon;
 			}
 		});
 		publish.IncidentObjectList.forEach(function(IncidentObject) {
-			var data = dataBase.IncidentObjectStore[IncidentObject.ID];
+			var data = scope.IncidentObjectStore[IncidentObject.ID];
 			if (data) {
 				updateProperties(IncidentObject, data);
 			} else {
-				dataBase.IncidentObjectStore[IncidentObject.ID] = IncidentObject;
+				scope.IncidentObjectStore[IncidentObject.ID] = IncidentObject;
 			}
 		});
 		publish.MediaList.forEach(function(Media) {
-			var data = dataBase.MediaStore[Media.ID];
+			var data = scope.MediaStore[Media.ID];
 			if (data) {
 				updateProperties(Media, data);
 			} else {
-				dataBase.MediaStore[Media.ID] = Media;
+				scope.MediaStore[Media.ID] = Media;
 			}
 		});
 		publish.MessengerList.forEach(function(Messenger) {
-			var data = dataBase.MessengerStore[Messenger.ID];
+			var data = scope.MessengerStore[Messenger.ID];
 			if (data) {
 				updateProperties(Messenger, data);
 			} else {
-				dataBase.MessengerStore[Messenger.ID] = Messenger;
+				scope.MessengerStore[Messenger.ID] = Messenger;
 			}
 		});
 		publish.PatientList.forEach(function(Patient) {
-			var data = dataBase.PatientStore[Patient.ID];
+			var data = scope.PatientStore[Patient.ID];
 			if (data) {
 				updateProperties(Patient, data);
 			} else {
-				dataBase.PatientStore[Patient.ID] = Patient;
+				scope.PatientStore[Patient.ID] = Patient;
 			}
 		});
 		publish.ResourceMobilizationList.forEach(function(ResourceMobilization) {
-			var data = dataBase.ResourceMobilizationStore[ResourceMobilization.ID];
+			var data = scope.ResourceMobilizationStore[ResourceMobilization.ID];
 			if (data) {
 				updateProperties(ResourceMobilization, data);
 			} else {
-				dataBase.ResourceMobilizationStore[ResourceMobilization.ID] = ResourceMobilization;
+				scope.ResourceMobilizationStore[ResourceMobilization.ID] = ResourceMobilization;
 			}
 		});
 		publish.ResourceStatusList.forEach(function(ResourceStatus) {
-			var data = dataBase.ResourceStatusStore[ResourceStatus.ID];
+			var data = scope.ResourceStatusStore[ResourceStatus.ID];
 			if (data) {
 				updateProperties(ResourceStatus, data);
 			} else {
-				dataBase.ResourceStatusStore[ResourceStatus.ID] = ResourceStatus;
+				scope.ResourceStatusStore[ResourceStatus.ID] = ResourceStatus;
 			}
 		});
 	}
 
-	// TODO SQLITE3333
+	// Construct a wonderful transaction to store in the database
+	var transactionToStore = new protoTransaction();
+	transactionToStore.SenderID = ""; // space efficienty
+	transactionToStore.PublishList = new protoTransaction.Content();
+
+	transactionToStore.PublishList.HelpBeaconList = StoreToList(scope.HelpBeaconStore);
+	transactionToStore.PublishList.IncidentObjectList = StoreToList(scope.IncidentObjectStore);
+	transactionToStore.PublishList.MediaList = StoreToList(scope.MediaStore);
+	transactionToStore.PublishList.MessengerList = StoreToList(scope.MessengerStore);
+	transactionToStore.PublishList.PatientList = StoreToList(scope.PatientStore);
+	transactionToStore.PublishList.ResourceMobilizationList = StoreToList(scope.ResourceMobilizationStore);
+	transactionToStore.PublishList.ResourceStatusList = StoreToList(scope.ResourceStatusStore);
+
+	// Store it in the database <3
+	insertDb.run(+new Date, transactionToStore.toBuffer());
 });
