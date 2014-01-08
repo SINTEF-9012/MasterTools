44a8d0a9e118c3354e4806c3b06635d7b3606633
Antoine Pultier <antoine.pultier@sintef.no>
2014-01-08 11:08:35 +0100

    Stuff

---
+++ Arma2Master/Arma2MasterConnector/LatLngInterpolation.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arma2MasterConnector
+{
+    class LatLngInterpolation
+    {
+        public class Point
+        {
+            public double Lat;
+            public double Lng;
+            public double X;
+            public double Y;
+        }
+
+        // No navigable map in C# :/
+        protected List<Point> Points = new List<Point>();
+
+        public void AddPoint(double lat, double lng, double x, double y)
+        {
+            Points.Add(new Point
+                {
+                    Lat = lat,
+                    Lng = lng,
+                    X = x,
+                    Y = y
+                });
+        }
+
+        public void AddPoint(Point p)
+        {
+            Points.Add(p);
+        }
+
+        public Point ToLatLng(double x, double y)
+        {
+            Point pointXSup = null,
+                pointXInf = null,
+                pointYSup = null,
+                pointYInf = null;
+
+            // Looking for nearest points in O(n)
+            foreach (var point in Points)
+            {
+                if (point.X < x && (pointXInf == null || point.X > pointXInf.X))
+                {
+                    pointXInf = point;
+                }
+                if (point.X > x && (pointXSup == null || point.X < pointXSup.X))
+                {
+                    pointXSup = point;
+                }
+                if (point.Y < y && (pointYInf == null || point.Y > pointYInf.Y))
+                {
+                    pointYInf = point;
+                }
+                if (point.Y > y && (pointYSup == null || point.Y < pointYSup.Y))
+                {
+                    pointYSup = point;
+                }
+            }
+
+            if (pointXSup == null || pointXInf == null || pointYInf == null || pointYSup == null)
+            {
+                throw new Exception("LatLngInterpolation: X and Y out of bounds");
+            }
+
+            return new Point
+                {
+                    X = x,
+                    Y = y,
+                    // Linear interpolation
+                    Lng = pointXInf.Lng + (x - pointXInf.X) * ((pointXSup.Lng - pointXInf.Lng) / (pointXSup.X - pointXInf.X)),
+                    Lat = pointYInf.Lat + (y - pointYInf.Y) * ((pointYSup.Lat - pointYInf.Lat) / (pointYSup.Y - pointYInf.Y))
+                };
+        }
+
+        public Point ToXY(double lat, double lng)
+        {
+            Point pointLatSup = null,
+                pointLatInf = null,
+                pointLngSup = null,
+                pointLngInf = null;
+
+            // Looking for nearest points in O(n)
+            foreach (var point in Points)
+            {
+                if (point.Lat < lat && (pointLatInf == null || point.Lat > pointLatInf.Lat))
+                {
+                    pointLatInf = point;
+                }
+                if (point.Lat > lat && (pointLatSup == null || point.Lat < pointLatSup.Lat))
+                {
+                    pointLatSup = point;
+                }
+                if (point.Lng < lng && (pointLngInf == null || point.Lng > pointLngInf.Lng))
+                {
+                    pointLngInf = point;
+                }
+                if (point.Lng > lng && (pointLngSup == null || point.Lng < pointLngSup.Lng))
+                {
+                    pointLngSup = point;
+                }
+            }
+
+            if (pointLatSup == null || pointLatInf == null || pointLngInf == null || pointLngSup == null)
+            {
+                throw new Exception("LatLngInterpolation: X and Y out of bounds");
+            }
+
+            return new Point
+            {
+                Lat = lat,
+                Lng = lng,
+                // Linear interpolation
+                X = pointLatInf.X + (lng - pointLatInf.Lng) * ((pointLatSup.X - pointLatInf.X) / (pointLatSup.Lng - pointLatInf.Lng)),
+                Y = pointLngInf.Y + (lat - pointLngInf.Lat) * ((pointLngSup.Y - pointLngInf.Y) / (pointLngSup.Lat - pointLngInf.Lat))
+            };
+        }
+    }
+}
--- Arma2Master/Arma2MasterConnector/Program.cs
+++ Arma2Master/Arma2MasterConnector/Program.cs
@@ -36,7 +36,36 @@ namespace Arma2MasterConnector
             Client.Connect();
 
             var server = new TcpListener(IPAddress.Any, 7845);
+
+            var interpolation = new LatLngInterpolation();
+            //interpolation.AddPoint(39.883154, 25.223207, 13859, 13433);
+            //interpolation.AddPoint(39.988642, 25.036114, 2066, 22463);
+            //interpolation.AddPoint(39.831807, 25.053073, 2854, 9390);
+
+            var start = DotSpatial.Projections.KnownCoordinateSystems.Geographic.World.WGS1984;
+            var end = DotSpatial.Projections.KnownCoordinateSystems.Projected.World.WebMercator;
+
+            var projection = new double[2] { 39.905572, 25.221947};
             
+            DotSpatial.Projections.Reproject.ReprojectPoints(projection, null, start, end, 0, 1);
+            /*Console.WriteLine(a[0] + " === "+a[1]);
+            a[0] += 1000;
+            DotSpatial.Projections.Reproject.ReprojectPoints(a, null, end, start, 0, 1);
+            Console.WriteLine(a[0] + " === " + a[1]);
+
+            interpolation.AddPoint(39.785307, 25.35754, 22315, 5101);
+            interpolation.AddPoint(39.905572, 25.221947, 14258, 15819);
+            interpolation.AddPoint(39.871295, 25.051542, 2837, 12671);
+            interpolation.AddPoint(39.790971, 25.236764, 14536, 5780);
+            interpolation.AddPoint(39.96116, 25.273177, 17191, 19864);
+
+            var p = interpolation.ToLatLng(14000, 9000);
+            var c = interpolation.ToXY(p.Lat, p.Lng);
+            Console.WriteLine(c.X + " -- "+c.Y);
+            Console.ReadLine();
+
+            return;*/
+
             server.Start();
 
 
@@ -50,7 +79,7 @@ namespace Arma2MasterConnector
                     
                     var stream = new StreamReader(client.GetStream());
 
-                    var centerLocation = new Location();
+                    //var centerLocation = new Location();
 
                     while (client.GetStream().CanRead)
                     {
@@ -61,13 +90,17 @@ namespace Arma2MasterConnector
 
                         if (message.StartsWith("INIT:"))
                         {
-                            centerLocation = new Location(message.Substring(5));
+                            /*centerLocation = new Location
+                                {
+                                    lat=39.883162,
+                                    lng=25.223201
+                                };*/
                             continue;
                         }
 
 
                         // TODO send scripts
-                        //w.WriteLine("hint \"Vive les canards\";");
+                        //w.WriteLine("group abcd addWaypoint [[14352,15000,100],0] setWayPointType \"MOVE\";");
                         //w.Flush();
 
                         IList<object> arguments;
@@ -91,25 +124,48 @@ namespace Arma2MasterConnector
                             var patient = new NodeMaster.PatientModel();
                             patient.ID = (string)argument[0];
 
+
+
                             /*lat = 42.507847,//59.944821,*
                                     lng = 5.973579//10.712956*/
 
-                            var location = new Location(centerLocation);
+                            /*var location = new Location(centerLocation);
+
+                            location.Move(270, 13856.3*(1/0.75));
+                            location.Move(180, 13433.7 * (1 / 0.75));*/
 
                             var x = (double) argument[1];
                             var y = (double) argument[2];
 
-                            location.Move(
-                                Math.Tan(x/y)*57.2957795,
-                                Math.Sqrt(x*x + y*y)
-                                );
+                            //22315, 5101
+                            //14258, 15819
+
+                            var plusX = (x - 14258) / 0.75;
+                            var plusY = (y - 15819) / 0.75;
+
+                            var p = new double[2] {plusY + projection[0], plusX + projection[1]};
+
+                            DotSpatial.Projections.Reproject.ReprojectPoints(p, null, end, start, 0, 1);
+
+                            /*location.Move(
+                                (Math.Tan(x/y)*57.2957795)%360,
+                                Math.Sqrt(x * x + y * y) * (1 / 0.75)
+                                );*/
+                            //var location = interpolation.ToLatLng(x, y);
 
                             patient.Location = new LatLng()
                             {
-                                lat = location.lat,
-                                lng = location.lng
+                                lat = p[0],
+                                lng = p[1]
                             };
 
+                            //Console.WriteLine(x + " canard "+y + " => " + Math.Sqrt(x*x+y*y) + " ou "+centerLocation.DistanceTo(location));
+
+                            /*if (patient.ID.Contains("antoinep"))
+                            {
+                                Console.WriteLine("X: " + location.X+"\tY:"+location.Y);
+                                Console.WriteLine("Lat: " + location.Lat + "\tLng:" + location.Lng);
+                            }*/
                             transaction.PublishList.PatientList.Add(patient);
                         }
 
